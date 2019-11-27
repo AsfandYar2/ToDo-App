ab165030cde33dddb6be73d25b3cb22bebf7a3d3
Asfand Yar <asfi.official@gmail.com>
2019-11-27 20:24:53 +0500

    Updates

---
+++ precache-manifest.5c452a3e85999a8d5128866f92489442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f8b4833d062503524580c8c2d1d8a1",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "1185d7d1c28d17b9275b",
+    "url": "/todo-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "3f30d6ac85b4245923b4",
+    "url": "/todo-app/static/css/main.e4d4ec3c.chunk.css"
+  },
+  {
+    "revision": "1185d7d1c28d17b9275b",
+    "url": "/todo-app/static/js/2.02302935.chunk.js"
+  },
+  {
+    "revision": "3f30d6ac85b4245923b4",
+    "url": "/todo-app/static/js/main.d2b8965b.chunk.js"
+  },
+  {
+    "revision": "89a13b256b803674333e",
+    "url": "/todo-app/static/js/runtime-main.6b7a25b3.js"
+  }
+]);
